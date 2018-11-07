9126b55a5647902a332f2ef9495c05db3324fe32
Saran Tanpituckpong <sarunta@gmail.com>
2018-11-07 23:38:34 +0700

    :beers: Export both forms

--- src/index.ts
+++ src/index.ts
@@ -2,4 +2,5 @@ import Icon from './types/Icon';
 
 const icons: Icon[] = require('../character-list/character-list.json').icons;
 
-export = icons as Icon[];
+module.exports = icons;
+export default icons as Icon[];
